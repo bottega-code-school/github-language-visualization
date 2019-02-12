97e7b17a3af68b7bd06d4a60107ab4e5912c8c14
jordanhudgens <jordan@devcamp.com>
2019-02-12 13:16:50 -0600

    Built out the App component and built the graph data object along with the call to the Vuex store to retrieve the github repo data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <graph v-if="currentRepoData.length > 0" v-bind="graphData" />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+import Graph from "./components/Graph";
+
+export default {
+  name: "App",
+
+  components: {
+    Graph
+  },
+
+  mounted() {
+    this.getData('jordanhudgens');
+  },
+
+  computed: {
+    ...mapGetters([
+      'currentRepoData',
+    ]),
+
+    graphData() {
+      return {
+        xAxisLabel: 'Month',
+        yAxisLabel: 'Project Count',
+        xKey: 'date',
+        yKey: 'value',
+        interval: 'month',
+        data: this.currentRepoData
+      }
     }
+  },
+
+
+  methods: {
+    ...mapActions([
+      'getData'
+    ]),
   }
 }
+</script>
+
+<style lang="scss">
+@import "./styles/main.scss";
 </style>
