789d285a22e523ff7142c77fd8ead1d61a72cc3d
jordanhudgens <jordan@devcamp.com>
2019-02-15 15:41:20 -0600

    Added additional profile data and styles

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="profile-wrapper">
-    <div class="profile-card">
-      <div class="avatar">
+    <div v-if="profile" class="profile-card">
+      <div v-if="profile.avatar_url" class="avatar">
         <img :src="profile.avatar_url" alt="avatar">
       </div>
 
+      <div class="user-data">
+        <div class="name">
+          <a :href="profile.html_url" target="_blank">{{profile.name}}</a>
+        </div>
+
+        <div class="bio">
+          {{profile.bio}}
+        </div>
+      </div>
+
       <pre>{{profile}}</pre>
+    </div>
+
+    <div v-else class="loading-card">
+      <i class="fas fa-spinner fa-spin"></i>
     </div>
   </div>
 </template>
@@ -16,11 +30,6 @@ export default {
 
   props: {
     profile: Object
-  },
-
-  data() {
-    return {
-    }
   }
 }
 </script>
