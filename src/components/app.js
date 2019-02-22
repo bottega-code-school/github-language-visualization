95f79ac4215766c70d7fad94f0d00c3ee84dffe5
jordanhudgens <jordan@devcamp.com>
2019-02-21 16:21:56 -0800

    Switched from a stacked area chart to a bar chart to better visualize the amount of data

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { AreaChart } from "react-d3-components";
+import d3 from "d3";
+import { BarChart } from "react-d3-components";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -37,27 +38,25 @@ export default class App extends Component {
         {
           label: "somethingA",
           values: [
-            { x: 0, y: 2 },
-            { x: 1.3, y: 5 },
-            { x: 3, y: 6 },
-            { x: 3.5, y: 6.5 },
-            { x: 4, y: 6 },
-            { x: 4.5, y: 6 },
-            { x: 5, y: 7 },
-            { x: 5.5, y: 8 }
+            { x: "SomethingA", y: 10 },
+            { x: "SomethingB", y: 4 },
+            { x: "SomethingC", y: 3 }
           ]
         },
         {
           label: "somethingB",
           values: [
-            { x: 0, y: 3 },
-            { x: 1.3, y: 4 },
-            { x: 3, y: 7 },
-            { x: 3.5, y: 8 },
-            { x: 4, y: 7 },
-            { x: 4.5, y: 7 },
-            { x: 5, y: 7.8 },
-            { x: 5.5, y: 9 }
+            { x: "SomethingA", y: 6 },
+            { x: "SomethingB", y: 8 },
+            { x: "SomethingC", y: 5 }
+          ]
+        },
+        {
+          label: "somethingC",
+          values: [
+            { x: "SomethingA", y: 6 },
+            { x: "SomethingB", y: 8 },
+            { x: "SomethingC", y: 5 }
           ]
         }
       ]
@@ -103,11 +102,10 @@ export default class App extends Component {
           <Search handleUsernameSearch={this.handleUsernameSearch} />
           <Profile profile={this.state.profileData} />
 
-          <AreaChart
+          <BarChart
             data={this.state.chartData}
             width={1200}
             height={500}
-            yOrientation="right"
             tooltipHtml={this.tooltipConfig}
             margin={{ top: 10, bottom: 50, left: 50, right: 10 }}
           />
