a4ebe7449378b1bf2697a1d11a948a1d76f8d723
jordanhudgens <jordan@devcamp.com>
2019-02-23 16:12:52 -0800

    Added the keys so they can be accessed via the server file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
+const keys = require("./config/keys");
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
 app.listen(port);
 
 console.log("server started");
+console.log("process.env ID", process.env.GITHUB_CLIENT_ID);
+console.log("process.env Secret", process.env.GITHUB_CLIENT_SECRET);
+
+console.log("keys", keys);
